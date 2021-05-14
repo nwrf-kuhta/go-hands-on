a85a497bd014c1188894d84c9311a86b1bc858c9
kuhta <nwrf.kuhta@gmail.com>
2021-05-14 20:56:15 +0900

    List2-11

---
+++ src/list2_11/list2-11.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	x := input("type a number: ")
+	n, err := strconv.Atoi(x)
+	if err == nil {
+		fmt.Print("1から" + x + "の合計は、")
+	} else {
+		return
+	}
+	t := 0
+	for i := 1; i <= n; i++ {
+		t += i
+	}
+	fmt.Println(t, "です。")
+}
+
+func input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(msg)
+	scanner.Scan()
+	return scanner.Text()
+}
