14889b53e40e2cc35a825476dd89aca36cf50b62
kuhta <nwrf.kuhta@gmail.com>
2021-05-15 16:38:05 +0900

    List2-25

---
+++ src/list2_25/list2-25.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 名前付き戻り値の利用
+func main() {
+	m := []string{
+		"one", "two", "three",
+	}
+	fmt.Println(m)
+	m = insert(m, "*", 2)
+	m = insert(m, "*", 1)
+	fmt.Println(m)
+}
+
+func insert(a []string, v string, p int) (s []string) {
+	s = append(a, "")
+	s = append(s[:p+1], s[p:len(s)-1]...)
+	s[p] = v
+	return
+}
