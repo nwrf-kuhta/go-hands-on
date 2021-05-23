c31a0337f259790fad5be1845dbaa1e7b6796e5e
kuhta <nwrf.kuhta@gmail.com>
2021-05-23 22:00:33 +0900

    List3-12

---
+++ src/list3_12/list3-12.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type intp int
+
+// 素数かを返す
+func (num intp) IsPrime() bool {
+	n := int(num)
+	for i := 2; i <= (n / 2); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// 素因数分解した結果を出力する
+func (num intp) PrimeFactor() []int {
+	ar := []int{}
+	x := int(num)
+	n := 2
+	for x > n {
+		if x%n == 0 {
+			x /= n
+			ar = append(ar, n)
+		} else {
+			if n == 2 {
+				n++
+			} else {
+				n += 2
+			}
+		}
+	}
+	ar = append(ar, x)
+	return ar
+}
+
+// 既存の型を拡張する
+func main() {
+	s := input("type a number: ")
+	n, _ := strconv.Atoi(s)
+	x := intp(n)
+	fmt.Printf("%d [%t].\n", x, x.IsPrime())
+	fmt.Println(x.PrimeFactor())
+	x *= 2
+	x++
+	fmt.Printf("%d [%t].\n", x, x.IsPrime())
+	fmt.Println(x.PrimeFactor())
+}
+
+func input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(msg)
+	scanner.Scan()
+	return scanner.Text()
+}
